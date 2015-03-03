02061dabff6ab138c912ef557c771be483c1dfbd
Arissa "Reck" Brown <flipmodes01@gmail.com>
2015-03-03 11:24:38 -0500

    Testing headers

--- app/app.module.js
+++ app/app.module.js
@@ -6,14 +6,29 @@
     app.controller('teamList', function ($scope, $http) {
 
         //$scope.list = 'test';
+        var req = {
+            method: 'GET',
+            url: 'https://www.bungie.net/platform/Group/289784/MembersV3/?currentPage=1&itemsPerPage=50',
+            headers: {
+                'X-API-Key': '1bb92ab568d446da9d90af616d961472'
+            },
+            data: { test: 'test' },
+        }
 
-        $http.get('https://www.bungie.net/platform/Group/289784/MembersV3/?currentPage=1&itemsPerPage=50').
+        $http(req).success(function(data){
+            $scope.data = data.Response;
+        }).error(function(data, status){
+            $scope.status = status;
+            console.log($scope.status);
+        });
+
+        /*$http.get('https://www.bungie.net/platform/Group/289784/MembersV3/?currentPage=1&itemsPerPage=50').
         success(function (data) {
             $scope.data = data.Response;
         }).
         error(function (data, status) {
             $scope.status = status;
-        });
+        });*/
 
     });
 
